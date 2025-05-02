56a3d7c970c2710d3050cc5c2a5d613979f886d2
Mrks <github@mburm.de>
2025-05-02 14:20:15 +0200

    Restructured overworld location jsons.

--- scripts/locations_import.lua
+++ scripts/locations_import.lua
@@ -1,13 +1,15 @@
+-- access
 Tracker:AddLocations("locations/access/Blossom.json")
+Tracker:AddLocations("locations/access/Crimson.json")
 Tracker:AddLocations("locations/access/Fall.json")
 Tracker:AddLocations("locations/access/Forbidden.json")
-Tracker:AddLocations("locations/access/Goals.json")
 Tracker:AddLocations("locations/access/Hills.json")
 Tracker:AddLocations("locations/access/Lake.json")
 Tracker:AddLocations("locations/access/Pillar.json")
-Tracker:AddLocations("locations/access/Crimson.json")
 Tracker:AddLocations("locations/access/Swamp.json")
 Tracker:AddLocations("locations/access/Temple.json")
+
+-- objects
 --Tracker:AddLocations("locations/types/bosses.json")
 --Tracker:AddLocations("locations/types/checkpoints.json")
 --Tracker:AddLocations("locations/types/coins.json")
@@ -15,15 +17,28 @@ Tracker:AddLocations("locations/access/Temple.json")
 --Tracker:AddLocations("locations/types/shards.json")
 --Tracker:AddLocations("locations/types/skills.json")
 --Tracker:AddLocations("locations/types/talismans.json")
+
+-- areas
 Tracker:AddLocations("locations/areas/Blossom.json")
+Tracker:AddLocations("locations/areas/Crimson.json")
 Tracker:AddLocations("locations/areas/Fall.json")
 Tracker:AddLocations("locations/areas/Forbidden.json")
-Tracker:AddLocations("locations/areas/Goals.json")
 Tracker:AddLocations("locations/areas/Hills.json")
 Tracker:AddLocations("locations/areas/Lake.json")
 Tracker:AddLocations("locations/areas/Pillar.json")
-Tracker:AddLocations("locations/areas/Crimson.json")
 Tracker:AddLocations("locations/areas/Swamp.json")
 Tracker:AddLocations("locations/areas/Temple.json")
-Tracker:AddLocations("locations/Overworld.json")
+
+-- overworld
+Tracker:AddLocations("locations/overworld/Blossom.json")
+Tracker:AddLocations("locations/overworld/Crimson.json")
+Tracker:AddLocations("locations/overworld/Fall.json")
+Tracker:AddLocations("locations/overworld/Forbidden.json")
+Tracker:AddLocations("locations/overworld/Hills.json")
+Tracker:AddLocations("locations/overworld/Lake.json")
+Tracker:AddLocations("locations/overworld/Pillar.json")
+Tracker:AddLocations("locations/overworld/Swamp.json")
+Tracker:AddLocations("locations/overworld/Temple.json")
+
+-- legend
 Tracker:AddLocations("locations/Legend.json")
